b8b6b29614cbdf29b50bda935234eb6c8183dad9
MMMPeter <minmg.peter@gmail.com>
2019-07-27 19:13:16 +0630

    test 4

---
+++ www/src/pages/TempDetail.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-ons-page>
+    <custom-toolbar v-bind="toolbarInfo"></custom-toolbar>
+    <custom-detail v-bind="toolbarInfo"></custom-detail>
+
+  <h3>welcome to detail page</h3>  
+  </v-ons-page>
+</template>
+
+<script>
+export default {
+  props: ['title']
+};
+
+</script>
+
+<style>
+
+</style>
