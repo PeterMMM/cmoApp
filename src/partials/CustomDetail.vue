0c11079ce34488d773e853e49003e336078fb7f9
MMMPeter <minmg.peter@gmail.com>
2019-07-28 10:00:23 +0630

    add config 101

---
+++ src/partials/CustomDetail.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-ons-card>
+    <img v-bind:src="url"  alt="Image Url" style="width: 100%;height: auto;">
+    <div class="title">
+      {{title}}
+    </div>
+    <div class="content">
+      <p>
+        {{des}}
+      </p>
+      <div>
+        <ons-button><ons-icon icon="ion-thumbsup"></ons-icon></ons-button>
+        <ons-button><ons-icon icon="ion-share"></ons-icon></ons-button>
+      </div>
+    </div>
+  </v-ons-card>
+</template>
+
+<script>
+export default {
+  props: ['title','des' , 'url','backLabel']
+};
+</script>
